54deaa707db468759fcf011896afad809c4da73d
MdMuntakimHye <46520159+MdMuntakimHye@users.noreply.github.com>
2023-03-18 22:11:32 +0600

    Update Feed.js

--- components/Feed.js
+++ components/Feed.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import { handlePostState, useSSRPostsState } from '@/atoms/postAtom';
+import React, { useEffect, useState } from 'react'
+import { useRecoilState } from 'recoil';
 import Input from './Input'
+import Post from './Post';
 
-function Feed() {
+function Feed({posts}) {
+  const [realtimePosts,setRealtimePosts] = useState([]);
+  const [handlePost,setHandlePost] = useRecoilState(handlePostState);
+  const [useSSRPosts,setUseSSRPosts] = useRecoilState(useSSRPostsState);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/api/posts", {
+        method: "GET",
+        headers: {"Content-Type": "application/json"}
+      });
+      const responseData = await response.json();
+      setRealtimePosts(responseData);
+      setHandlePost(false);
+      setUseSSRPosts(false);
+    }
+    fetchPosts();
+  },[handlePost]);
   return (
     <div className="space-y-6 pb-24 max-w-lg">
+      <Input />
         {/* posts */}
-        <Input />
+        {!useSSRPosts
+        ? realtimePosts.map((post) => <Post key={post._id} post={post}/>)
+        : posts.map((post) => <Post key={post._id} post={post}/>)}
+        {/* server side */}
+        
     </div>
   )
 }
